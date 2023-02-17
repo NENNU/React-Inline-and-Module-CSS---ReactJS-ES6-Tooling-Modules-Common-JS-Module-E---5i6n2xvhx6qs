0c7b4513bcfaefb8e6f7ad3fad34cc5f6b6df4ed
Neha kashyap <32489573+NENNU@users.noreply.github.com>
2023-02-17 20:56:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,10 @@ const heading ={
 }
   return (
     <div id="main">
-      <h1 >Success</h1>
-      <h1 id='error' >Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      <h1 id='error' style={{fontSize:"50px",color:"red"}}>Error</h1>
     </div>
   )
 }
+export default App;
 
-
-export default App;
