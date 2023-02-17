a7f0afbaa6cae198532d26fa7ebf1dba442672d0
Neha kashyap <32489573+NENNU@users.noreply.github.com>
2023-02-17 20:50:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react'
+import '../styles/App.css';
+import styles from '../styles/appStyles.module.css';
+const App = () => {
+const heading ={
 
 
+}
+  return (
+    <div id="main">
+      <h1 className={styles.success}>Success</h1>
+      <h1 id='error' style={{fontSize:"50px",color:"red"}}>Error</h1>
+    </div>
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
